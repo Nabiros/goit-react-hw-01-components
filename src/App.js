ad711bba40f56c7e8ae120ceacca9cc6a14e5fbe
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-08-31 11:37:26 +0300

    setted components & added data

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// import user from './data/user.json';
+// import statisticalData from './data/statistical-data.json';
+// import friends from './data/friends.json';
+// import transactions from './data/transactions.json';
+
+// import SocialProfile from './components/SocialProfile/SocialProfile.jsx';
+// import Statistics from './components/Statistics/Statistics.jsx';
+// import FriendList from './components/FriendList/FriendList.jsx';
+// import TransactionHistory from './components/TransactionHistory/TransactionHistory.jsx';
