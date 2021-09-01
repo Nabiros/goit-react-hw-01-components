0e94fb8f3827d870f9e0b0067899ea3442390bbc
Nazar Khizhnyak <77328105+Nabiros@users.noreply.github.com>
2021-09-01 14:04:28 +0300

    added social profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-// import user from './data/user.json';
+import user from './data/user.json';
 // import statisticalData from './data/statistical-data.json';
 import friends from './data/friends.json';
 // import transactions from './data/transactions.json';
 
-// import SocialProfile from './components/SocialProfile/SocialProfile.jsx';
+import SocialProfile from './components/SocialProfile/SocialProfile.jsx';
 // import Statistics from './components/Statistics/Statistics.jsx';
 import FriendList from './components/FriendList/FriendList.jsx';
 // import TransactionHistory from './components/TransactionHistory/TransactionHistory.jsx';
@@ -11,15 +11,17 @@ import FriendList from './components/FriendList/FriendList.jsx';
 export default function App() {
   return (
     <div>
-      {/* <SocialProfile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        followers={user.stats.followers}
-        likes={user.stats.likes}
-        views={user.stats.views}
-      /> */}
+      {
+        <SocialProfile
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          followers={user.stats.followers}
+          likes={user.stats.likes}
+          views={user.stats.views}
+        />
+      }
 
       {/* <Statistics title="Upload stats" stats={statisticalData} /> */}
       {/* <Statistics stats={statisticalData} /> */}
---
+++ src/components/SocialProfile/SocialProfile.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import styled from './SocialProfile.module.css';
+
+export default function SocialProfile(props) {
+  const { avatar, name, tag, location, followers, views, likes } = props;
+
+  return (
+    <div className={styled.profile}>
+      <div className={styled.description}>
+        <img src={avatar} alt="Аватар пользователя" className={styled.avatar} width="100" />
+        <p className={styled.name}>{name}</p>
+        <p className={styled.tag}>@{tag}</p>
+        <p className={styled.location}>{location}</p>
+      </div>
+
+      <ul className={styled.stats}>
+        <li className={styled.items}>
+          <span className={styled.label}>Followers</span>
+          <span className={styled.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={styled.label}>Views</span>
+          <span className={styled.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={styled.label}>Likes</span>
+          <span className={styled.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+SocialProfile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  followers: PropTypes.string.isRequired,
+  views: PropTypes.string.isRequired,
+  likes: PropTypes.string.isRequired,
+};
