768724486b50775941d9f6cc64216821171a2005
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-09-03 12:11:21 +0300

    added transaction

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import user from './data/user.json';
 import statisticalData from './data/statistical-data.json';
 import friends from './data/friends.json';
-// import transactions from './data/transactions.json';
+import transactions from './data/transactions.json';
 
 import SocialProfile from './components/SocialProfile/SocialProfile.jsx';
 import Statistics from './components/Statistics/Statistics.jsx';
 import FriendList from './components/FriendList/FriendList.jsx';
-// import TransactionHistory from './components/TransactionHistory/TransactionHistory.jsx';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory.jsx';
 
 export default function App() {
   return (
@@ -26,7 +26,7 @@ export default function App() {
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
-      {/* <TransactionHistory items={transactions} /> */}
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
